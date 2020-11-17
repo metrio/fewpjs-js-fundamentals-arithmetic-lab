e36d5c7e9c57e8b249f736a324ac6563518b7bc0
metrio <dlima.nyit@gmail.com>
2020-11-17 18:47:24 -0500

    commit tree 4738e15f6db1b0d3eda7e9b09a608ced36d4feff

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2
+
+const random = Math.random()
+
+const mod = 16 % 6
+
+const max =  Math.max( 1, 7, 5, 12 ,16, 20)
